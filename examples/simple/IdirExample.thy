c3a411183340c2f7108b694506ad87a3b017ec2e
Idir AIT SADOUNE <idir.aitsadoune@centralesupelec.fr>
2018-04-06 14:25:21 +0200

    no message

--- examples/simple/IdirExample.thy
+++
@@ -1,9 +0,0 @@
-theory IdirExample
-  imports  "../../ontologies/ScientificPaperOnto"
-
-begin
-
-text*[p1::Paragraph] {* ceci est un paragraphe *}
-text*[abs::Abstract, content = p1]{* *}
-
-end
---
+++ ontologies/mathex_onto.thy
@@ -0,0 +1,6 @@
+theory mathex_onto
+ 
+imports "../Isa_DOF"
+begin
+
+end
