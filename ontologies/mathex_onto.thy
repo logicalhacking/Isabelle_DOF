9ee8811576445060c1491c23b177060c3dfdb0cc
Idir AIT SADOUNE <idir.aitsadoune@centralesupelec.fr>
2018-04-06 14:32:22 +0200

    no message

--- ontologies/mathex_onto.thy
+++ ontologies/mathex_onto.thy
@@ -3,4 +3,15 @@ theory mathex_onto
 imports "../Isa_DOF"
 begin
 
+doc_class Question =
+  content :: "string list"
+
+doc_class Response =
+  content :: "string list"
+
+doc_class Exercise = 
+  question :: Question
+  response :: Response
+
+
 end
