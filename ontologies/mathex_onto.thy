d3d7bd760972fc04443f363ecee73c96d695d3fc
Idir AIT SADOUNE <idir.aitsadoune@centralesupelec.fr>
2018-04-17 16:42:45 +0200

    no message

--- ontologies/mathex_onto.thy
+++ ontologies/mathex_onto.thy
@@ -3,33 +3,34 @@ theory mathex_onto
 imports "../Isa_DOF"
 begin
 
+doc_class Author =
+   affiliation :: "string"
+
+datatype Subject =
+  algebra | geometry | statistical
+
+datatype Level =
+  oneStar | twoStars | threeStars
+
+datatype Grade =
+  A1 | A2 | A3
+
+doc_class Header = 
+  examGrade :: Grade
+  examSubject :: Subject
+
 doc_class Question =
-  content :: "(string + term) list"
-
-doc_class Response =
-  content :: "(string + term) list"
-
-datatype grades = A | B | C  
-  
-doc_class Exercise_part = 
-  question :: Question
-  response :: Response
+  level :: Level
+  mark :: integer
 
 doc_class Exercise= 
-  content :: "(Exercise_part) list"
+  content :: "(Question) list"
 
 doc_class MathExam=
-  content :: "(Exercise) list"
-
-section{* Example*}  
- 
-term "[ @{thm ''refl''}] @ [ @{thm ''sym''}, @{thm ''trans''} ] "
-  
-text{*
-      @{term "[ @{thm ''refl''}] @ [ @{thm ''sym''}, @{thm ''trans''} ]"}} are the theorems
-of the equational logic fragment of HOL.
-*}
-
+  content :: "(Header + Author + Exercise) list"
+ where "(Header ~ 
+        (Author)+  ~
+        (Exercise)+ )"
   
   
 end
