88231c6898bdb124a6bd947eb7ff2f0b269f3dc8
Achim D. Brucker <adbrucker@0x5f.org>
2019-08-03 12:03:09 +0100

    Cleanup.

--- examples/technical_report/Isabelle_DOF-Manual/Isabelle_DOF-Manual.thy
+++ examples/technical_report/Isabelle_DOF-Manual/Isabelle_DOF-Manual.thy
@@ -2,23 +2,10 @@
 theory "Isabelle_DOF-Manual"
   imports "05_Implementation"
 begin
-(*<*) 
-
-
-
-
-text*[bib::bibliography]\<open>References\<close>
-
-(*<*)
-
 close_monitor*[this]
-
 check_doc_global
-
 text\<open>Resulting trace in doc\_item ''this'': \<close>
 ML\<open>@{trace_attribute this}\<close>
-
-
 end
 (*>*) 
   
