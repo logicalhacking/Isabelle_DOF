df12a32624e841f8e92bbd3eba746f407646c08c
Achim D. Brucker <adbrucker@0x5f.org>
2019-08-04 08:24:01 +0100

    Added missing space to warning messages.

--- src/DOF/Isa_DOF.thy
+++ src/DOF/Isa_DOF.thy
@@ -1654,9 +1654,9 @@ fun check_and_mark ctxt cid_decl (str:{strict_checking: bool}) pos name  =
          in () end
     else if   DOF_core.is_declared_oid_global name thy 
          then (if #strict_checking str 
-               then warning("declared but undefined document reference:"^name)(* HACK bu 29/6.19 *)
+               then warning("declared but undefined document reference: "^name) (* HACK bu 29/6.19 *)
                else ())
-         else error("undefined document reference:"^name)
+         else error("undefined document reference: "^name)
   end
 
 val _ =  check_and_mark : Proof.context ->  string ->  {strict_checking: bool} ->
