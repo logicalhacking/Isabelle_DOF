9d7fc957c67368fd27f363f137c0c0a3efd76a0b
Burkhart Wolff <wolff@lri.fr>
2022-01-26 11:17:22 +0100

    new syntax for paper. And the future.

--- src/tests/Ontology_Matching_Example.thy
+++ src/tests/Ontology_Matching_Example.thy
@@ -9,44 +9,44 @@ text\<open>Using HOL, we can define a mapping between two ontologies.
   Here is an example which show how to map two classes.
   HOL also allows us to map the invariants (ontological rules) of the classes!\<close>
 
-type_synonym UTF8 = string
+type_synonym UTF8 = string        \<comment> \<open>for the sake of the demonstration\<close>
 
 definition utf8_to_string
-  where "utf8_to_string x = x"
+  where "utf8_to_string x = x"    \<comment> \<open>for the sake of the demonstration\<close>
 
 
 doc_class A =
-  first_name :: UTF8
-  last_name :: UTF8
-  age :: nat
-  married_to :: "string option"
+  first_name  :: UTF8
+  last_name   :: UTF8
+  age         :: nat
+  married_to  :: "string option"
   invariant a :: "age \<sigma> < 18 \<longrightarrow> married_to \<sigma> = None"
+
 doc_class B =
-  name :: string 
-  adult :: bool
-  is_married :: bool
+  name        :: string 
+  adult       :: bool
+  is_married  :: bool
   invariant b :: "is_married \<sigma> \<longrightarrow> adult \<sigma>"
 
-text\<open>We define the mapping between the two classes,
-  i.e. how to transform the class @{doc_class A} in to the class @{doc_class B}:\<close>
+text\<open>We define the mapping between the two classes,  i.e. how to transform 
+     the class @{doc_class A} in to the class @{doc_class B}:\<close>
 
-definition A_to_B_morphism
-  where "A_to_B_morphism X =
-        \<lparr> tag_attribute = A.tag_attribute X
-          , name = utf8_to_string (first_name X) @ '' ''  @ utf8_to_string (last_name X)
-          , adult = (age X \<ge> 18)
-          , is_married = (married_to X \<noteq> None) \<rparr>" 
+
+definition A_to_B_mapping :: "'a A_scheme \<Rightarrow> B" ("_ \<langle>B\<rangle>\<^sub>A" [1000]999)
+  where "\<sigma> \<langle>B\<rangle>\<^sub>A =
+        \<lparr> tag_attribute = A.tag_attribute \<sigma>
+          , name = utf8_to_string (first_name \<sigma>) @ '' ''  @ utf8_to_string (last_name \<sigma>)
+          , adult = (age \<sigma> \<ge> 18)
+          , is_married = (married_to \<sigma> \<noteq> None) \<rparr>" 
 
 text\<open>Now we check that the invariant is preserved through the morphism:\<close>
 
 lemma inv_a_preserved :
-  "a_inv X \<Longrightarrow> b_inv (A_to_B_morphism X)"
-  unfolding a_inv_def b_inv_def A_to_B_morphism_def
-  by auto
+  "a_inv \<sigma> \<Longrightarrow> b_inv (\<sigma> \<langle>B\<rangle>\<^sub>A)"
+  unfolding a_inv_def b_inv_def A_to_B_mapping_def  by auto
 
 lemma A_morphism_B_total :
-  "A_to_B_morphism ` ({X::A. a_inv X}) \<subseteq> ({X::B. b_inv X})"
-  using inv_a_preserved
-  by auto
+  "A_to_B_mapping ` ({X::A. a_inv X}) \<subseteq> ({X::B. b_inv X})"
+  using inv_a_preserved by auto
 
 end
