33d991a1c77337c2eaef08d81baf0b9cb256e496
Nicolas Méric <nicolasmeric@posteo.net>
2022-02-04 17:21:26 +0100

    Update OntoMathPro_Ontology.thy

--- src/tests/OntoMathPro_Ontology.thy
+++ src/tests/OntoMathPro_Ontology.thy
@@ -23,13 +23,15 @@ datatype annotation = DC dc | OWL owl |  RDFS rdfs
 onto_class Top =
   Annotations :: "annotation list"
 
-onto_class Field_of_mathematics = 
+onto_class Field_of_mathematics =
   Annotations :: "annotation list"
-  invariant restrict_annotation ::"set (Annotations \<sigma>) \<subseteq>
-                                              range (RDFS o label) \<union> range (OWL o versionInfo)"
+  invariant restrict_annotation_F ::"set (Annotations \<sigma>) \<subseteq>
+                                              range (RDFS o label) \<union> range (RDFS o comment)"
 
-onto_class Mathematical_knowledge_object = 
+onto_class Mathematical_knowledge_object =
   Annotations :: "annotation list"
+  invariant restrict_annotation_M ::"set (Annotations \<sigma>) \<subseteq>
+                                              range (RDFS o label) \<union> range (RDFS o comment)"
 
 onto_class assoc_F_M =
   contains:: "(Field_of_mathematics \<times> Mathematical_knowledge_object) set"
@@ -52,9 +54,9 @@ onto_class assoc_M_M' =
                                     \<longrightarrow> (\<exists> y \<in> Range (defines \<sigma>). (x, y) \<in> defines \<sigma>)"
 
 onto_class assoc_M_M_see_also =
-  see_also :: "(Mathematical_knowledge_object \<times> Mathematical_knowledge_object) set"
-  invariant see_also_defined :: "\<forall> x. x \<in> Domain (see_also \<sigma>)
-                                    \<longrightarrow> (\<exists> y \<in> Range (see_also \<sigma>). (x, y) \<in> see_also \<sigma>)"
+  see_also :: "(Mathematical_knowledge_object rel) set" 
+  invariant see_also_trans :: "\<forall> r \<in> (see_also \<sigma>). trans r"
+  invariant see_also_sym :: "\<forall> r \<in> (see_also \<sigma>). sym r"
 
 onto_class Problem = Mathematical_knowledge_object +
   Annotations :: "annotation list"
@@ -71,5 +73,3 @@ onto_class assoc_Method_Problem =
   solves :: "(Method \<times> Problem) set"
   invariant solves_defined :: "\<forall> x. x \<in> Domain (solves \<sigma>)
                                     \<longrightarrow> (\<exists> y \<in> Range (solves \<sigma>). (x, y) \<in> solves \<sigma>)"
-
-
