7b4450450dc75d43ce01ef8847fdbcf3710eab21
Achim D. Brucker <adbrucker@0x5f.org>
2022-03-27 12:02:15 +0100

    Hide use of build script from users.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Added
 
 ### Changed
+ 
+- removed explicit use of build script. Requires removing "build" script entry
+  from ROOT files.
+
 
 ## 1.2.0 - 2022-03-26
 
--- src/scala/dof_document_build.scala
+++ src/scala/dof_document_build.scala
@@ -1,7 +1,32 @@
-/*  Author:     Makarius
-
-Build theory document (PDF) from session database.
-*/
+/*
+ * Copyright (c) 
+ *               2021-2022 The University of Exeter. 
+ *               2021-2022 The University of Paris-Saclay. 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
 package isabelle_dof
 
@@ -32,6 +57,9 @@ object DOF_Document_Build
             .write(directory.doc_dir)
         }
       }
+      val dof_home= Path.explode(Isabelle_System.getenv_strict("ISABELLE_DOF_HOME"));
+      // print(context.options.string("dof_url"));
+      Isabelle_System.copy_file(dof_home + Path.explode("src/scripts/build"), directory.doc_dir);
 
       directory
     }
