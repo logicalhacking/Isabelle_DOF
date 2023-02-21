f66b6187f8607f65ffe4ba22a31a2c2f21fd320a
Achim D. Brucker <adbrucker@0x5f.org>
2023-02-21 22:34:30 +0000

    Introduced use_ontology_unchecked (for internal use only).

--- Isabelle_DOF-Ontologies/document_setup.thy
+++ Isabelle_DOF-Ontologies/document_setup.thy
@@ -5,7 +5,9 @@ theory "document_setup"
 begin
 
 use_template "scrreprt-modern"
-use_ontology "technical_report"
+(* use_ontology "technical_report" *)
+use_ontology_unchecked "Isabelle_DOF.technical_report" and "Isabelle_DOF-Ontologies.CENELEC_50128"
+
 (*>*)
 
 title*[title::title]         \<open>Isabelle/DOF\<close>
