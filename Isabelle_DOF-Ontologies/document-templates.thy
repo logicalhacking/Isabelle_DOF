acb82477b56782dc14c8e91e841c25e00253d77f
Achim D. Brucker <adbrucker@0x5f.org>
2023-02-25 11:01:39 +0000

    Moved currently unsupported document templates to the Isabelle_DOF-Ontologies session.

---
+++ Isabelle_DOF-Ontologies/document-templates.thy
@@ -0,0 +1,24 @@
+(*************************************************************************
+ * Copyright (C) 
+ *               2019      The University of Exeter 
+ *               2018-2019 The University of Paris-Saclay
+ *               2018      The University of Sheffield
+ *
+ * License:
+ *   This program can be redistributed and/or modified under the terms
+ *   of the 2-clause BSD-style license.
+ *
+ *   SPDX-License-Identifier: BSD-2-Clause
+ *************************************************************************)
+
+theory 
+  "document-templates"
+imports
+  "Isabelle_DOF.Isa_DOF"
+begin
+
+define_template "./document-templates/root-eptcs-UNSUPPORTED.tex"
+define_template "./document-templates/root-lipics-v2021-UNSUPPORTED.tex"
+define_template "./document-templates/root-svjour3-UNSUPPORTED.tex"
+
+end
