ddcfb5f708f66acb7fb6f81d2f330316d7debd38
Achim D. Brucker <adbrucker@0x5f.org>
2023-06-01 00:21:19 +0200

    Initial commit: stubs for using beamer.

---
+++ Isabelle_DOF-Examples-Extra/beamerx/poster/poster.thy
@@ -0,0 +1,24 @@
+(*<*)
+theory "poster"
+  imports "Isabelle_DOF.scholarly_paper"
+ "Isabelle_DOF-Ontologies.document_templates"
+begin
+
+use_template "beamerposter-UNSUPPORTED"
+use_ontology "scholarly_paper"
+
+title*[tit::title]\<open>Example Presentation\<close>
+                                  
+author*[safouan,email="\<open>example@example.org\<close>",affiliation="\<open>Example Org\<close>"]\<open>Eliza Example\<close>
+(*>*)
+
+text\<open>
+\begin{frame}
+\frametitle{Example Slide}
+\centering\huge This is an example!
+\end{frame}
+\<close>
+
+(*<*)
+end 
+(*>*)
